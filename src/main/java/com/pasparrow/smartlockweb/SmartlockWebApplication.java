6ecebb435ebf9288983b4cd8ba9153517c4dbcec
unknown <bowwow1994@mail.hongik.ac.kr>
2021-04-27 19:21:27 +0900

    init

---
+++ src/main/java/com/pasparrow/smartlockweb/SmartlockWebApplication.java
@@ -0,0 +1,13 @@
+package com.pasparrow.smartlockweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartlockWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartlockWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pasparrow/smartlockweb/SmartlockWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pasparrow.smartlockweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartlockWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
